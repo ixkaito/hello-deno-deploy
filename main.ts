98463bdaa6d9fc496d121747602ad657f94e697c
Kite <ixkaito@gmail.com>
2021-03-30 17:37:13 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,16 +1,17 @@
 addEventListener('fetch', (event) => {
-const response = new Response(`
-<html>
-<head>
-  <title>Hello, world! by KITERETZ inc. from Deno Deploy</title>
-</head>
-<body>
-  Hello, world!<br>
-  by <a href="https://kiteretz.com/about/">KITERETZ inc.</a>
-</body>
-</html>
-`, {
-    headers: { 'content-type': 'text/plain' },
+  const html = `<html>
+    <head>
+      <title>Hello, world! by KITERETZ inc. from Deno Deploy</title>
+    </head>
+    <body>
+      Hello, world!<br>
+      by <a href="https://kiteretz.com/about/">KITERETZ inc.</a>
+    </body>
+  </html>`;
+  
+  const response = new Response(html, {
+    headers: { 'content-type': 'text/html; charset=UTF-8' },
   });
+  
   event.respondWith(response);
 });
